8291ef0157fb0426f65f867e59416c71a54671a5
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2021-09-19 02:29:26 +1200

    Added constants file

---
+++ Handler/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_CS.Handler
+{
+    static class Constants
+    {
+        public const double MELEE_CRIT_CHANCE = 0.05;
+        public const double RANGED_CRIT_CHANCE = 0.21;
+        public const double SPELL_CRIT_CHANCE = 0.05;
+        public const double RECURSIVE_CRIT_CHANCE = 0.02;
+
+        public const double RECURSIVE_HIT_CHANCE = 0.7;
+
+    }
+}
