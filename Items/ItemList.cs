4fac7664dee297277f8b212bfd177f0a277bef62
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2021-09-20 14:35:04 +1200

    Create static list of items

---
+++ Items/ItemList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_CS.Items
+{
+    static class MeleeWeaponList
+    {
+        public static MeleeWeapon WoodSword = new MeleeWeapon("Wooden Sword", 2, 0);
+    }
+
+    static class RangedWeaponList
+    {
+        public static RangedWeapon WoodBow = new RangedWeapon("Wooden Bow", 3, 0);
+    }
+
+    static class MagicWeaponList
+    {
+        public static MagicWeapon WoodStaff = new MagicWeapon("Wooden Staff", 3, 0);
+    }
+}
